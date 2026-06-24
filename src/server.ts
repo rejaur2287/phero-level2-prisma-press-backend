6c8445690f39ff046cf539eb8373f789d9664110
rejaur2287 <rezaurewubd@gmail.com>
2026-06-24 06:29:02 +0600

    Configuring Environment Variables With dotenv And Using Express Built In Middlewares In app.ts

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import app from "./app";
+import config from "./config";
+
+const PORT = config.port;
+
+async function main() {
+  try {
+    // await prisma.$connect();
+    console.log("Connected to the database.");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}.`);
+    });
+  } catch (error) {
+    console.error("Error starting server:", error);
+    // await prisma.$disconnect();
+    process.exit(1);
+  }
+}
+
+main();
