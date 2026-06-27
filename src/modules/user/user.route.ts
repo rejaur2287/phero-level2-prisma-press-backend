94518eb055f534d0ab1eec19dc2e01ebaa108ccf
rejaur2287 <rezaurewubd@gmail.com>
2026-06-27 14:52:59 +0600

    Cleaning And Organizing The Auth Middleware And Implementing Update User Profile API

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,75 +1,11 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { Router } from "express";
 import { userController } from "./user.controller";
-import { jwtUtils } from "../../utils/jwt";
-import config from "../../config";
 import { Role } from "../../../generated/prisma/enums";
-import httpStatus from "http-status";
-import { catchAsync } from "../../utils/catchAsync";
-import { JwtPayload } from "jsonwebtoken";
-import { prisma } from "../../lib/prisma";
+import { auth } from "../../middlewares/auth";
 
 const router = Router();
 
-declare global {
-  namespace Express {
-    interface Request {
-      user?: {
-        id: string;
-        email: string;
-        name: string;
-        role: Role;
-      };
-    }
-  }
-}
-
 router.post("/register", userController.registerUser);
-
-// auth(Role.ADMIN, Role.USER, Role.AUTHOR)
-// auth()=> ...requiredRoles=> [Role.ADMIN, Role.USER, Role.AUTHOR]
-
-const auth = (...requiredRoles: Role[]) => {
-  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.cookies.accessToken;
-    // || req.headers.authorization?.startsWith("Bearer")
-    //   ? req.headers.authorization?.split(" ")[1]
-    //   : req.headers.authorization;
-
-    if (!token) {
-      throw new Error(
-        "You are not logged in! Please log in to get access this resource.",
-      );
-    }
-
-    const verifiedToken = jwtUtils.verifyToken(token, config.jwt_access_secret);
-
-    if (!verifiedToken.success) {
-      throw new Error(verifiedToken.error);
-    }
-
-    const { id, email, name, role } = verifiedToken.data as JwtPayload;
-
-    if (requiredRoles.length && !requiredRoles.includes(role)) {
-      throw new Error(
-        "Forbidden: You do not have permission to access this resource",
-      );
-    }
-
-    const user = await prisma.user.findUnique({
-      where: { id, email, name, role },
-    });
-
-    if (!user) {
-      throw new Error("User not found. Please log in again.");
-    }
-
-    if (user.activeStatus === "BLOCKED") {
-      throw new Error("Your account has been blocked. Please contact support.");
-    }
-    req.user = { id, email, name, role };
-    next();
-  });
-};
 
 router.get(
   "/me",
@@ -108,6 +44,12 @@ router.get(
 
   auth(Role.ADMIN, Role.USER, Role.AUTHOR),
   userController.getMyProfile,
+);
+
+router.put(
+  "/my-profile",
+  auth(Role.ADMIN, Role.USER, Role.AUTHOR),
+  userController.updateMyProfile,
 );
 
 export const userRoutes = router;
