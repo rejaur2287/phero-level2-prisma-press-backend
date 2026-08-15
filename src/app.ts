321d7bc363488bc6c4f8a08287e2d621f8c01c93
rejaur2287 <rezaurewubd@gmail.com>
2026-08-15 17:38:51 +0600

    Implementing Not Found Route And Global Error Handler

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import cookieParser from "cookie-parser";
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import config from "./config";
 import { userRoutes } from "./modules/user/user.route";
 import { authRoutes } from "./modules/auth/auth.routes";
 import { postsRoutes } from "./modules/post/post.route";
 import { commentsRoutes } from "./modules/comment/comment.route";
+import { notFound } from "./middlewares/notFound";
+import httpStatus from "http-status";
+import { globalErrorHandler } from "./middlewares/globalErrorHandler";
 
 const app: Application = express();
 
@@ -29,5 +32,27 @@ app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postsRoutes);
 app.use("/api/comments", commentsRoutes);
+
+// app.use((req: Request, res: Response) => {
+//   res.status(404).json({
+//     message: "Route not found",
+//     path: req.originalUrl,
+//     date: Date(),
+//   });
+// });
+
+app.use(notFound);
+
+// app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+//   console.log(err);
+//   res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+//     success: false,
+//     statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+//     message: err.message,
+//     error: err.stack,
+//   });
+// });
+
+app.use(globalErrorHandler);
 
 export default app;
